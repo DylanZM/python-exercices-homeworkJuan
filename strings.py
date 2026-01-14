63fc07fd0ee6687e28fda243d5e7291658ee02da
DylanZM <dylanleandro31@gmail.com>
2026-01-14 19:56:07 -0400

    feat: string exercice

--- strings.py
+++ strings.py
@@ -1,3 +1,8 @@
 text : str = "Hello, World!"
 
-print(text)
+print(text.upper())
+print(text.lower())
+print(text.find("d"))
+new_text = text.replace("World", "Python")
+print(text, new_text)
+print("World" in text)
